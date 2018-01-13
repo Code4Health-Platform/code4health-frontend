4b9c962a26fb2e558eb470948553aecb3093ad89
james-weaver <james.weaver@gmail.com>
2018-01-13 16:56:01 +0000

    Simple welcome page

--- app/src/components/pages/Welcome/Welcome.js
+++ app/src/components/pages/Welcome/Welcome.js
@@ -1,11 +1,7 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
 
 export const Welcome = (props) => (
-  <div>
-    <h1>Welcome to code4health</h1>
-    <Link to={`/log-in`}>log in</Link>
-  </div>
+  <h1>Welcome to code4health</h1>
 )
 
 Welcome.propTypes = {
