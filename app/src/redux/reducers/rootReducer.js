62e015284a010a3b442bcd884577063a7eff93e7
james-weaver <james.weaver@gmail.com>
2018-01-14 12:53:55 +0000

    Clear state on unauthenticated

--- app/src/redux/reducers/rootReducer.js
+++ app/src/redux/reducers/rootReducer.js
@@ -1,12 +1,24 @@
+/* global localStorage */
+
 import { combineReducers } from 'redux'
 import { reducer as formReducer } from 'redux-form'
 import authReducer from '@reducers/auth'
+import * as constants from '@actions/auth/constants'
 import dashboardReducer from '@reducers/dashboard'
 
-const rootReducer = combineReducers({
+const appReducer = combineReducers({
   form: formReducer,
   auth: authReducer,
   dashboard: dashboardReducer
 })
+
+const rootReducer = (state, action) => {
+  if (action.type === constants.UNAUTHENTICATED) {
+    state = undefined
+    localStorage.removeItem('user')
+  }
+
+  return appReducer(state, action)
+}
 
 export default rootReducer
