53d77f3e179165f6c6518c66cc91be34715917ba
james-weaver <james.weaver@gmail.com>
2018-01-14 13:01:57 +0000

    allow HMR to accept `authRequired` component

--- app/src/components/Router.js
+++ app/src/components/Router.js
@@ -1,16 +1,18 @@
 import React, {Component} from 'react'
+import { connect } from 'react-redux'
 import {BrowserRouter, Route} from 'react-router-dom'
 
+import AuthRequired from '@components/auth/AuthRequired'
 import Footer from '@organisms/Footer'
 import NavigationBar from '@organisms/NavigationBar'
-
 import Welcome from '@pages/Welcome'
 import Dashboard from '@pages/Dashboard'
 import LogIn from '@pages/LogIn'
 import LogOut from '@pages/LogOut'
 import SignUp from '@pages/SignUp'
 
-import AuthRequired from '@components/auth/AuthRequired'
+const AuthenticatedDashboard = AuthRequired(Dashboard)
+const ConnectedDashboard = connect()(AuthenticatedDashboard)
 
 export default class App extends Component {
   render () {
@@ -22,7 +24,7 @@ export default class App extends Component {
           <Route exact path="/log-in" component={LogIn} />
           <Route exact path="/log-out" component={LogOut} />
           <Route exact path="/sign-up" component={SignUp} />
-          <Route exact path="/dashboard" component={AuthRequired(Dashboard)} />
+          <Route exact path="/dashboard" component={ConnectedDashboard} />
           <Footer />
         </div>
       </BrowserRouter>
