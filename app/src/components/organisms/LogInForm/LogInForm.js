0549d60c33dac3f32a987bd5117fa7836aca0d79
james-weaver <james.weaver@gmail.com>
2018-02-11 01:53:10 +0000

    Add missing form fields + localisation to login form

--- app/src/components/organisms/LogInForm/LogInForm.js
+++ app/src/components/organisms/LogInForm/LogInForm.js
@@ -1,52 +1,56 @@
 import React, { Component } from 'react'
 import {Field, reduxForm} from 'redux-form'
 import PropTypes from 'prop-types'
-
 import {Button, Input} from '@atoms'
+import {injectIntl} from 'react-intl'
 
 class LoginForm extends Component {
-  render () {
-    const {handleSubmit} = this.props
-
-    const usernameField = (
+  usernameField () {
+    return (
       <Field
         name='username'
         component={field =>
           <Input
+            label={this.props.intl.formatMessage({id: 'auth.logIn.form.username.label'})}
             type='text'
             placeholder='username'
             content={field.input.value}
-            onChangeHander={param =>
+            onChangeHandler={param =>
               field.input.onChange(param)
             }
           />
         }
       />
     )
+  }
 
-    const passwordField = (
+  passwordField () {
+    return (
       <Field
         name='password'
         component={field =>
           <Input
+            label={this.props.intl.formatMessage({id: 'auth.logIn.form.password.label'})}
             type='password'
             placeholder='password'
             content={field.input.value}
-            onChangeHander={param =>
+            onChangeHandler={param =>
               field.input.onChange(param)
             }
           />
         }
       />
     )
+  }
 
+  render () {
     return (
-      <form onSubmit={handleSubmit(this.props.formHandler)}>
+      <form onSubmit={this.props.handleSubmit(this.props.formHandler)}>
         {this.props.errorMessage}
 
-        {usernameField}
+        {this.usernameField()}
 
-        {passwordField}
+        {this.passwordField()}
 
         <Button type='submit'>
           Log In
@@ -54,6 +58,10 @@ class LoginForm extends Component {
 
         <Button variant='muted' href={`/sign-up`}>
           Not yet registered? Sign up
+        </Button>
+
+        <Button variant='muted' href={`/forgot-password`}>
+          Forgotten your password?
         </Button>
       </form>
     )
@@ -63,7 +71,8 @@ class LoginForm extends Component {
 LoginForm.propTypes = {
   formHandler: PropTypes.func,
   handleSubmit: PropTypes.any,
-  errorMessage: PropTypes.string
+  errorMessage: PropTypes.string,
+  intl: PropTypes.any
 }
 
 const reduxFormLogin = reduxForm({
@@ -71,4 +80,4 @@ const reduxFormLogin = reduxForm({
   fields: ['username', 'password', 'confirm-password']
 })(LoginForm)
 
-export default reduxFormLogin
+export default injectIntl(reduxFormLogin)
