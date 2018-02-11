11a0dde5851eeaa06ebc9d343554507b1e43fc07
james-weaver <james.weaver@gmail.com>
2018-02-11 01:53:27 +0000

    Export password reset page

--- app/src/components/pages/index.js
+++ app/src/components/pages/index.js
@@ -2,6 +2,7 @@ import Account from '@pages/Account'
 import Dashboard from '@pages/Dashboard'
 import LogIn from '@pages/LogIn'
 import LogOut from '@pages/LogOut'
+import PasswordReset from '@pages/PasswordReset'
 import SignUp from '@pages/SignUp'
 import Welcome from '@pages/Welcome'
 
@@ -10,6 +11,7 @@ export {
   Dashboard,
   LogIn,
   LogOut,
+  PasswordReset,
   SignUp,
   Welcome
 }
