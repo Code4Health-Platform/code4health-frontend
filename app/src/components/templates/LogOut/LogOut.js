c1336631df243904398c6eda17baf5fd00992aa1
james-weaver <james.weaver@gmail.com>
2018-03-04 17:29:57 +0000

    Add masthead to logout page

--- app/src/components/templates/LogOut/LogOut.js
+++ app/src/components/templates/LogOut/LogOut.js
@@ -1,41 +1,35 @@
 import React from 'react'
 import {Grid} from 'grid-styled'
-import {Alert, Heading, Button, Paragraph} from '@atoms'
+import {Alert, Heading, Button} from '@atoms'
+import {Masthead} from '@molecules'
 import PropTypes from 'prop-types'
 
-const LogOut = props => {
-  if (props.authenticated) {
-    return (
-      <div>
-        <Grid w={1} p={1}>
-          <Heading level={2} icon='logout'>
-            Log Out
-          </Heading>
-          <Alert type='error' message={props.errorMessage} />
-        </Grid>
-        <Grid w={1} p={1}>
-          <Paragraph>Are you sure you want to log out?</Paragraph>
-          <Button
-            type='button'
-            clickHandler={() => props.logOutAction}
-            width='auto'
-          >
-            Log Out
-          </Button>
-        </Grid>
-      </div>
-    )
-  } else {
-    return (
-      <div>already logged out</div>
-    )
-  }
-}
+const LogOut = props => (
+  <div>
+    <Masthead>
+      <Heading level={2} icon='logout'>Log Out</Heading>
+    </Masthead>
+
+    <Alert type='error' message={props.errorMessage} />
+    <Alert type='success' message={props.successMessage} />
+
+    <Grid w={1} p='1rem'>
+      <Heading level={3}>Are you sure you want to log out?</Heading>
+      <Button
+        type='button'
+        clickHandler={() => props.logOutAction}
+        width='auto'
+      >
+        Log Out
+      </Button>
+    </Grid>
+  </div>
+)
 
 LogOut.propTypes = {
   errorMessage: PropTypes.string,
-  logOutAction: PropTypes.func,
-  authenticated: PropTypes.bool
+  successMessage: PropTypes.string,
+  logOutAction: PropTypes.func
 }
 
 export default LogOut
--- app/src/router/index.js
+++ app/src/router/index.js
@@ -6,13 +6,10 @@ import {Account, Activate, NewProject, LogIn, LogOut, Projects, SingleProject, S
 import History from '@history'
 
 const AuthenticatedProjects = AuthRequired(Projects)
-// const ConnectedProjects = connect()(AuthenticatedProjects)
 const AuthenticatedSingleProject = AuthRequired(SingleProject)
-// const ConnectedSingleProject = connect()(AuthenticatedSingleProject)
 const AuthenticatedNewProject = AuthRequired(NewProject)
-// const ConnectedNewProject = connect()(AuthenticatedNewProject)
 const AuthenticatedAccount = AuthRequired(Account)
-// const ConnectedAccount = connect()(AuthenticatedAccount)
+const AuthenticatedLogOut = AuthRequired(LogOut)
 
 class AppRouter extends Component {
   render () {
@@ -22,7 +19,7 @@ class AppRouter extends Component {
           <Route exact path='/' component={Welcome} />
           <Route exact path='/activate' component={Activate} />
           <Route exact path='/log-in' component={LogIn} />
-          <Route exact path='/log-out' component={LogOut} />
+          <Route exact path='/log-out' component={AuthenticatedLogOut} />
           <Route exact path='/sign-up' component={SignUp} />
           <Route exact path='/forgot-password' component={PasswordReset} />
           <Route exact path='/account' component={AuthenticatedAccount} />
