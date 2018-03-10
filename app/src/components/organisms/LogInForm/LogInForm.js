7ea708393fda408510fed08a78e524d584a0f364
james-weaver <james.weaver@gmail.com>
2018-03-10 14:44:07 +0000

    Social login buttons

--- app/src/components/organisms/LogInForm/LogInForm.js
+++ app/src/components/organisms/LogInForm/LogInForm.js
@@ -1,35 +1,66 @@
 import React, { Component } from 'react'
 import {Fields, reduxForm} from 'redux-form'
 import PropTypes from 'prop-types'
-import {Button, Input} from '@atoms'
+import {Button, FormSeparator} from '@atoms'
+import {FormInput} from '@molecules'
 
 const renderFields = (fields) => (
   <div>
-    <Input {...fields.username.input} type='text' placeholder='username' />
-    <Input {...fields.password.input} type='password' placeholder='password' />
+    <FormInput
+      type='text'
+      placeholder='Email Address'
+      label='Email address'
+      error={fields.username.meta.error} touched={fields.username.meta.touched}
+      onChangeHandler={fields.username.input.onChange}
+      onBlurHandler={fields.username.input.onBlur}
+      required
+    />
+
+    <FormInput
+      type='password'
+      placeholder='Password'
+      label='Password'
+      error={fields.password.meta.error} touched={fields.password.meta.touched}
+      onChangeHandler={fields.password.input.onChange}
+      onBlurHandler={fields.password.input.onBlur}
+      required
+    />
   </div>
 )
 
 class LoginForm extends Component {
   render () {
     return (
-      <form onSubmit={this.props.handleSubmit}>
-        {this.props.errorMessage}
-
-        <Fields names={['username', 'password']} component={renderFields} />
-
-        <Button type='submit'>
-          Log In
+      <div>
+        <Button type='' variant='github' icon='github'>
+          Log In with Github
         </Button>
 
-        <Button variant='muted' href={`/sign-up`}>
-          Not yet registered? Sign up
+        <Button type='' variant='google' icon='google'>
+          Log In with Google
         </Button>
 
-        <Button variant='muted' href={`/forgot-password`}>
-          Forgotten your password?
-        </Button>
-      </form>
+        <FormSeparator>or</FormSeparator>
+
+        <form onSubmit={this.props.handleSubmit}>
+
+          {this.props.errorMessage}
+
+          <Fields names={['username', 'password']} component={renderFields} />
+
+          <Button type='submit'>
+            Log In
+          </Button>
+
+          <Button variant='muted' href={`/sign-up`}>
+            Not yet registered? Sign up
+          </Button>
+
+          <Button variant='muted' href={`/forgot-password`}>
+            Forgotten your password?
+          </Button>
+        </form>
+      </div>
     )
   }
 }
